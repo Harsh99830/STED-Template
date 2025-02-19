d27f4b6e1b6079e41bf0e3d18d0833e3f7f9aa16
harsh agrawal <harshagrawal7878@gmail.com>
2025-02-19 18:27:01 +0530

    teacher button added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,8 @@ function Main() {
   return (
     <div>
       <h1>Start slide</h1>
-      <button><Link to={'/slide-1'}>Click here</Link></button>
+      <button><Link to={'/slide-1'}>For students</Link></button>
+      <button><Link to={'/teacher-slide-1'}>For teachers</Link></button>
     </div>
   )
 }
